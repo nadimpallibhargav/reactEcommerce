5991eb484a77a658a7268d33f532e9cabf1a1ad5
nadimpallibhargav <bhargavraju98@gmail.com>
2022-12-11 16:38:35 +0000

    search input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 import './header.scss';
 
-function getItem(label, key, children, type) {
+function getItem(label, key, items, type) {
     return {
       key,
-      children,
+      items,
       label,
       type,
     };
@@ -21,6 +21,10 @@ const items = [
     ]),
 ];
 
+const { Search } = Input;
+
+const onSearch = (value) => console.log(value);
+
 export const Header = () => {
     return(
         <header>
@@ -28,14 +32,47 @@ export const Header = () => {
                 <div className="logo">
                     MyBazaar
                 </div>
-                <Menu mode="horizontal">
-                    <Menu.SubMenu key="SubMenu" title="Shop By Category" icon={<MenuOutlined />}>
-                        <Menu
-                            mode="vertical"
-                            items={items}
-                        />
-                    </Menu.SubMenu>
-                </Menu>
+                <Row>
+                    <Col 
+                        xs={{
+                            span: 24,
+                        }}
+                        sm={{
+                            span: 24,
+                        }}
+                        lg={{
+                            span: 12,
+                        }}
+                        md={{
+                            span: 12,
+                        }}
+                    >                                              
+                        <Menu mode="horizontal">
+                            <Menu.SubMenu key="SubMenu" title="Shop By Category" icon={<MenuOutlined />}>
+                                <Menu
+                                    mode="vertical"
+                                    items={items}
+                                />
+                            </Menu.SubMenu>
+                        </Menu>                        
+                    </Col>
+                    <Col 
+                        xs={{
+                            span: 24,
+                        }}
+                        sm={{
+                            span: 24,
+                        }}
+                        lg={{
+                            span: 12,
+                        }}
+                        md={{
+                            span: 12,
+                        }}
+                    >                        
+                        <Search placeholder="Search for a Product" onSearch={onSearch} enterButton />
+                    </Col>
+                </Row>
             </div>
         </header>
     )
